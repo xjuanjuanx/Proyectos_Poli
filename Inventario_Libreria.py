661cce8c073516fa5320feecd7997ed26fc82f12
Juan Sebastian Velez <velezjuann258@gmail.com>
2025-11-11 15:32:08 -0500

    Refactor book inventory management logic

--- Inventario_Libreria.py
+++ Inventario_Libreria.py
@@ -1,11 +1,9 @@
-
 import os
 nombreLibro = []
 precioLibro = []
 cantidadLibros = []
 inventario1 = {}
 inventario2 = {}
-cont = 0
 
 while True:
     os.system("cls")
@@ -14,7 +12,7 @@ while True:
     match opc:
         case 1:
             print("\n=== REGISTRO DE LIBROS ===")
-            nombre = input("Ingrese el nombre del libro\n").lower()
+            nombre = input("Ingrese el nombre del libro\n").strip().lower()
             precio = float(input("Ingrese el precio del libro\n"))
             cantidad = int(input("Ingrese la cantidad que llego a la bodega\n"))
             if nombre in nombreLibro:
@@ -27,7 +25,6 @@ while True:
                 inventario1[nombre] = cantidad
                 inventario2[nombre] = precio
                 input("Libro agregado al sistema, Preciona una tecla para continuar")
-            cont += 1
         case 2:
             while True:
                 os.system("cls")
@@ -35,8 +32,7 @@ while True:
                 opc2 = int(input("1. Actualizar cantidad de libros\n2. Actualizar precio de libros\n3. Salir\n"))
                 match opc2:
                     case 1:
-                        nombreN = input("Ingrese el nombre del libro que desea modificar\n").lower()
-                        
+                        nombreN = input("Ingrese el nombre del libro que desea modificar\n").strip().lower()
                         if nombreN in inventario1:
                             cant = int(input("Ingrese una nueva cantidad para el inventario:\n"))
                             inventario1[nombreN] = cant
@@ -45,8 +41,8 @@ while True:
                             print("El libro no se encuentra en el sistema, Intente de nuevo")
                             input("Precione una tecla para continuar")
                     case 2:
-                        nombreN = input("Ingrese el nombre del libro que desea modificar\n").lower()
-                        if nombre in inventario2:
+                        nombreN = input("Ingrese el nombre del libro que desea modificar\n").strip().lower()
+                        if nombreN in inventario2:
                             val = int(input("Ingrese un nuevo valor para el inventario:\n"))
                             inventario2[nombreN] = val
                             input("Valor modificado, Precione una tecla para continuar")
@@ -63,9 +59,15 @@ while True:
                 print("Aun no hay datos registrados")
                 input("Precione una tecla para continuar")
             else:
-                for no, ca in inventario1.items():
-                    for no1, va in inventario2.items():
-                        print(f"{cont}. {no} tiene un Valor de {va:,.2f}, En el inventario hay {ca} libros de este ejemplar")
+                for nombre in nombreLibro:
+                    prec = ""
+                    canti = ""
+                    if nombre in inventario1:
+                        canti = inventario1[nombre]
+                    if nombre in inventario2:
+                        prec = inventario2[nombre]
+
+                        print(f"{nombre} tiene un Valor de {prec}, En el inventario hay {canti} libros de este ejemplar")
                 input("Precione una tecla para continuar")
         case 4:
             break
@@ -75,4 +77,5 @@ while True:
             
         
             
+
 
